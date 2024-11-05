f19ef78bac99674d4b43cb53a1660083a4e2ec61
emi web developer <marianseether@gmail.com>
2024-11-05 15:46:07 +0200

    node

---
+++ node_cli/file.js
@@ -0,0 +1,27 @@
+// const unu = () => doi()
+// const doi = () => trei()
+// const trei = () => patru()
+// const patru = () => {"asta e din functia patru"}
+
+// console.log(unu())
+
+
+// const unu = () => doi()
+// const doi = () => trei()
+// const trei = () => patru()
+// const patru = () => {
+// console.log("asta e din functia patru soto inteso...")
+//     return "asta e din functia patru"
+// }
+
+// unu()
+// console.log(unu())
+
+const one = () => doi
+const doi = () => {
+    console.log("functia doi...")
+    return "din functia doi"
+} 
+
+console.log(one())
+console.log(one()())
