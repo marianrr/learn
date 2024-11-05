67c0d58977071f57b77e41c5bf17f3ef5cd3e5b5
marianrr <marianseether@gmail.com>
2024-11-05 21:50:59 +0200

    cli-path

---
+++ module1/main.js
@@ -0,0 +1,20 @@
+﻿console.log('This is the module.')
+
+// mkdir module1
+// cd module1
+// set-content -path "main.js" -value "console.log('This is the module.')" -encoding utf8
+// code .
+
+// npm init --y
+// npm install colors --save-dev
+// npm uninstall colors
+
+// adaug
+// "start": "node main.js",
+// in
+// "scripts": {
+//     "start": "node main.js",
+//     "test": "echo \"Error: no test specified\" && exit 1"
+//   }
+
+//   npm start
