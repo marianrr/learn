43e86e2b53f3e56a03440b862cd8a985801a1d54
marianrr <marianseether@gmail.com>
2024-11-06 12:38:59 +0200

    class2

---
+++ class/main.js
@@ -0,0 +1,16 @@
+﻿
+function coco() {
+    this.name = "walter"
+    console.log("Name  is ", this.name, " .")
+    console.log("Name  is ", this)
+    console.log(this === coco)
+}
+
+coco()
+
+function so() {
+   
+    return "well"
+}
+
+console.log(so)
