3de43667c1b3f593b308412682d568a31e13dc1e
marianrr <marianseether@gmail.com>
2024-11-07 20:33:17 +0200

    console.time

---
+++ console.time/main.js
@@ -0,0 +1,21 @@
+function addNums(n) {
+    total = 0;
+  
+    for (let i = 1 ; i <= n ; i++) {
+      total += i;
+    }
+  
+    return total;
+  }
+
+//   console.time("addNums");
+// addNums(1000000);
+// console.timeEnd("addNums");
+
+startTime = Date.now();
+addNums(1000000);
+endTime = Date.now();
+
+console.log(startTime);  // 1608078573750
+console.log(endTime); 
+console.log(`Runtime: ${endTime - startTime}ms`)
