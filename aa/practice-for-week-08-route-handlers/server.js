da57d653aaad0e073007dd94e66051eaefa43580
emi web developer <marianseether@gmail.com>
2024-11-21 10:43:39 +0200

    dom

--- aa/practice-for-week-08-route-handlers/server.js
+++ aa/practice-for-week-08-route-handlers/server.js
@@ -16,9 +16,7 @@ const server = http.createServer((req, res) => {
     reqBody += data;
   });
 
-  // When the request is finished processing the entire body
   req.on("end", () => {
-    // Parsing the body of the request
     if (reqBody) {
       req.body = reqBody
         .split("&")
@@ -31,67 +29,71 @@ const server = http.createServer((req, res) => {
         }, {});
       console.log(req.body);
     }
-    // Do not edit above this line
 
-    // define route handlers here
+    if (req.method === "GET" && req.url === "/") {
+      const body = "Dog Club";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(body);
+    }
 
-if(req.method === "GET" && req.url === "/") {
-  const body = "Dog Club"
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.end(body)
-}
-if(req.method === "GET" && req.url === "/dogs") {
-  const body = "Dogs index"
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.write(body)
-  res.end()
-}
+    if (req.method === "GET" && req.url === "/dogs") {
+      const body = "Dogs index";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(body);
+    }
 
-if(req.method === "GET") {
-  let co = 0
-  let so = req.url.split("/")
-  if(so.length === 3) {co = so[2]
-    let body = `Dog details for dogId: ${co}`
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/plain")
-    res.end(body)
-}
-}
+    // if (req.method === "GET") {
+    //   const urlParts = req.url.split("/");
+    //   if (urlParts.length === 3 && urlParts[1] === "dogs") {
+    //     const dogId = urlParts[2];
+    //     const body = `Dog details for dogId: ${dogId}`;
+    //     res.statusCode = 200;
+    //     res.setHeader("Content-Type", "text/plain");
+    //     return res.end(body);
+    //   }
+    // }
 
-if(req.method === "GET" && req.url === "/dogs/new") {
-let body = "Dog create form page"
-res.statusCode = 200
-res.setHeader("Content-Type", "text/plain")
-res.end(body)
+    if (req.method === "GET" && req.url.startsWith("/dogs/new")) {
+      const body = "Dog create form page";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(body);
+    }
 
-}
+    // if (req.method === "POST" && req.url === "/dogs") {
+    //   const newDogId = getNewDogId();
+    //   res.statusCode = 302;
+    //   res.setHeader("Location", `/dogs/${newDogId}`);
+    //   return res.end();
+    // }
 
-if(req.method === "POST" && req.url === "/dogs"){
-res.statusCode = 302
-res.setHeader(`Location: '/dogs/${getNewDogId()}'`)
+    // if (req.method === "POST" && req.url.startsWith("/dogs/")) {
+    //   const urlParts = req.url.split("/");
+    //   const dogId = urlParts[2];
+    //   res.statusCode = 302;
+    //   res.setHeader("Location", `/dogs/${dogId}`);
+    //   return res.end();
+    // }
 
-res.end()
+    // if (req.method === "GET" && req.url.startsWith("/dogs/")) {
+    //   const urlParts = req.url.split("/");
+    //   const dogId = urlParts[2];
+    //   if (urlParts.length === 4 && urlParts[3] === "edit") {
+    //     const body = `Dog edit form page for dogId: ${dogId}`;
+    //     res.statusCode = 200;
+    //     res.setHeader("Content-Type", "text/plain");
+    //     return res.end(body);
+    //   }
+    // }
 
-}
-
-if()
-
-
-
-
-
-
-
-    // Do not edit below this line
-    // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('No matching route handler found for this endpoint');
+    res.setHeader("Content-Type", "text/plain");
+    return res.end("No matching route handler found for this endpoint");
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
