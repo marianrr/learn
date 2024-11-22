c20ba475a71e5ee2066e2161d12e4402bba9e654
emi web developer <marianseether@gmail.com>
2024-11-22 17:30:35 +0200

    hh

---
+++ practice-for-week-08-create-and-handle-promises/phase-1.js
@@ -0,0 +1,54 @@
+function stretch() {
+  
+return new Promise((resolve, reject) => {
+  setTimeout(() => resolve(), 1000)
+  console.log("done stretching")
+})
+}
+
+function runOnTreadmill() {
+  return new Promise((resolve, reject) => {
+setTimeout(() => resolve(), 500)
+console.log("done running on treadmill")
+})
+}
+
+function liftWeights() {
+  return new Promise((resolve, reject) => {
+setTimeout(() => resolve(), 2000)
+console.log("done lifting weights")
+  })
+}
+
+function workout() {
+  
+  // done stretching
+  // done running on treadmill
+  // done lifting weights
+  // done working out
+
+
+  stretch()
+  .then(runOnTreadmill)
+  .then(liftWeights)
+  .then(() => console.log("done working out"))
+
+
+
+
+}
+
+
+/* ============================ TEST YOUR CODE ============================
+
+Run the file (`node phase-1.js`) and check your output against the expected
+output.
+*/
+
+
+workout();
+  // should print out the following:
+    // done stretching
+    // done running on treadmill
+    // done lifting weights
+    // done working out
