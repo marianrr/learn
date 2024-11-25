7952ce7cded8f357d816a01193cdc09900f5796f
emi web developer <marianseether@gmail.com>
2024-11-26 00:02:42 +0200

    http

--- practice-for-week-08-fetch-from-server/assets/js/your-code.js
+++ practice-for-week-08-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,64 @@
+
+// sa testez doar o functie 
+//  npx jest -t "getAllDogs" 
+
 export function getAllDogs() {
-    // Your code here
+    const url = "/dogs"
+    return fetch(url)
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+
+    const url = "/dogs/2"
+    return fetch(url)
+
 }
 
+
 export function postNewDog() {
-    // Your code here
+    const url = "/dogs"
+    const headers = { "Content-Type": "application/x-www-form-urlencoded" }
+// sa fiu atent la URLSearchParams, o da vsc in balarii
+    const body = new URLSearchParams({ name: "coco", age: 44 })
+
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    }
+    return fetch(url, options)
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+
+    const url = "/dogs"
+    const headers = { "Content-Type": "application/x-www-form-urlencoded" }
+    const body = new URLSearchParams({
+        name: name, age: age
+    })
+
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    }
+    return fetch(url, options)
+
 }
 
 export function deleteDog(id) {
-      // Your code here
+
+    const url = `/dogs/${id}/delete`
+
+    const headers = {
+        "AUTH": "ckyut5wau0000jyv5bsrud90y"
+    }
+    const options = {
+        method: "POST",
+        headers: headers
+    }
+
+    return fetch(url, options)
+
+
 }
--- practice-for-week-08-fetch/fetch-requests.js
+++ practice-for-week-08-fetch/fetch-requests.js
@@ -7,18 +7,11 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
-fetch("http://localhost:5000/miau")
-.then(res => {
-    console.log("res este: ", res.status)
-    return res.text()
-})
-.then(data =>  { 
-    console.log("statusul este: ", data)
-    console.log("res este: ", res.status)
-
-})
-
-
+// fetch("http://localhost:5000")
+// .then(res => {
+//     console.log("res este: ", res.status)
+//     return res.text()
+// })
 
 
 /* ====== 2. Print true if the status of the response was successful ====== */
@@ -26,13 +19,35 @@ fetch("http://localhost:5000/miau")
 // Your code here
 
 
+// fetch("http://localhost:5000/soto")
+// .then(res => {
+//     if(res.ok) {
+//         console.log(true)
+//         return true
+//     }
+//     else {
+//         console.log(false)
+//         return false
+//     }
+// })
+
 
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
 
+// fetch("http://localhost:5000")
+// .then(res => {
+//     console.log(res.headers)
+//     console.log(res.headers.get("Content-Type"))
+// })
 
 
 /* ============== 4. Print the body of the response as text =============== */
 
 // Your code here
+
+
+fetch("http://localhost:5000")
+.then(res => res.text())
+.then(data => console.log(data))
