4c028e7123ac994cff5486eba10ab00fb12fb90c
emi web developer <marianseether@gmail.com>
2024-12-17 21:40:56 +0200

    lista5

--- express_mongo_3/app.js
+++ express_mongo_3/app.js
@@ -1,1 +1,40 @@
 const express = require("express")
+const { MongoClient } = require("mongodb")
+require("dotenv").config()
+const app = express()
+const PORT = process.env.PORT
+const uri = process.env.MONGODB_URI
+app.use(express.json())
+
+const client = new MongoClient(uri)
+
+// async function run() {
+//     try {
+//         const database = client.db('sample_mflix');
+//         const movies = database.collection('movies');
+//         // Query for a movie that has the title 'Back to the Future'
+//         const query = { title: 'Back to the Future' };
+//         const movie = await movies.findOne(query);
+//         console.log(movie);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir)
+
+
+app.get("/", async (req, res) => {
+    try {
+        const database =  client.db('sample_mflix');
+        const movies =  database.collection('movies');
+        const movie = await movies.find().limit(10).toArray();
+        console.log(movie);
+        res.status(200).json(movie)
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+})
+
+app.listen(PORT, () => console.log("starting..."))
