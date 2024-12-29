5b7a30d937bad3d15c5b3c63d1e3a0193178e6a4
emi web developer <marianseether@gmail.com>
2024-12-29 19:46:36 +0200

    lista5

--- form_table/main.js
+++ form_table/main.js
@@ -1,6 +1,6 @@
 const form1 = document.getElementById("form1")
 const table = document.getElementById("table1")
-
+const tr1 = document.querySelectorAll("tr")
 
 
 const submit1 = (e) => {
@@ -13,7 +13,15 @@ const submit1 = (e) => {
     console.log(id, name, location, age)
     const tr = document.createElement("tr")
     tr.innerHTML = `<td style="border:2px solid gray">${id}</td><td style="border:2px solid gray">${name}</td><td style="border:2px solid gray">${location}</td><td style="border:2px solid gray">${age}</td>`
+    tr.addEventListener("click", click1)
     table.appendChild(tr)
 }
 
+const click1 = (e) => {
+    console.log("nu nu nu chiu chiu...")
+    e.currentTarget.remove()
+
+}
+
 form1.addEventListener("submit", submit1)
+
