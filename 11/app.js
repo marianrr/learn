6d447faad81d137d5ad5b1e0b4de6c62be4cda94
marianrr <marianseether@gmail.com>
2025-01-16 22:11:21 +0200

    sql1

---
+++ 11/app.js
@@ -0,0 +1,67 @@
+const user = {
+    name: "Mili",
+    age: 22,
+    location: "Bucharest",
+    wage: 2000
+}
+
+const user1 = Object.getOwnPropertyNames(user)
+
+console.log(user1)
+console.table(user1)
+
+const user2 = [
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+    {
+        name: "Mili",
+        age: 22,
+        location: "Bucharest",
+        wage: 2000
+    },
+
+]
+
+const user3 = Object.getOwnPropertyNames(user2)
+console.log(user2)
+console.table(user2)
