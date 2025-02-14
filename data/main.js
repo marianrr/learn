faf0b207034b878c7f7ebc14cdc4855ce429e6ce
emi web developer <marianseether@gmail.com>
2025-02-14 08:05:52 +0200

    scope

---
+++ data/main.js
@@ -0,0 +1,25 @@
+// data types
+
+
+console.log(typeof 222)
+console.log(typeof "kikila")
+console.log(typeof 2244n)
+console.log(typeof "2244n")
+console.log(typeof {name: "Walter"})
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+
+const symbol1 = Symbol()
+console.log(typeof symbol1)
+
+console.log(typeof [1, 2, 3, 4])
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
+// console.log(typeof )
