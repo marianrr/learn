43056a83158fb46e69fb8c1a54af02d2732c8ebb
marianrr <marianseether@gmail.com>
2025-02-22 16:29:17 +0200

    interface

---
+++ interface1/main.js
@@ -0,0 +1,17 @@
+const Interface1 = {
+    delete1: function () {
+
+    },
+    display1: function () {
+
+    }
+}
+
+class User {
+    delete1() {
+        return "delete"
+    }
+    display1() {
+        return "display"
+    }
+}
