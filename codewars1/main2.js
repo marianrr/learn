2e003e6736fad4932318ba2606ca56c3ab0a5a26
marianrr <marianseether@gmail.com>
2025-02-27 23:04:30 +0200

    cw

--- codewars1/main2.js
+++ codewars1/main2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // let m = 3, n = 5;
 // let k = 1
 // let a = []
@@ -35,20 +34,3 @@
 // }
 
 // console.log(text1)
-=======
-function perimeter(n) {
-
-    const arr = [1, 1]
-    for (let i = 2; i <= n; i++) {
-        arr.push(arr[arr.length - 1] + arr[arr.length - 2])
-    }
-    console.log(arr)
-    return 4*arr.reduce((sum, num) => sum + num, 0)
-}
-
-console.log(perimeter(5))
-console.log(perimeter(5))
-console.log(perimeter(7))
-console.log(perimeter(20))
-console.log(perimeter(30))
->>>>>>> d6a3d0cf8f3f4621a31fe376238213d70592a831
