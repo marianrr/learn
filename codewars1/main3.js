e2a02db0a549b5e2962a063592f3653d36d42410
marianrr <marianseether@gmail.com>
2025-02-27 23:02:21 +0200

    cw

--- codewars1/main3.js
+++ codewars1/main3.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-function countFruits(halfFruits){
-    let arr = []
-    let lol = new Map()
-      for(let i = 0; i < halfFruits.length; i = i+2) {
-        if(halfFruits[i] < halfFruits[i+1]) continue
-        else {
-          const fruit = halfFruits[i] + halfFruits[i+1]
-          if(fruitsName.includes(fruit)) arr.push(fruit)
-        }
-        arr = arr.sort((a, b) => a-b)
-      console.log(arr)
-        for(let j = 0; j < arr.length; j++) {
-          let num = 0
-          if(arr[j] === arr[j+1]) num++
-          else {
-            lol.set(`${arr[j]}`, num)
-            num = 0
-          }
-        }
-      }
-      return lol
-    }
-    
-    
-    countFruits((["app","le","pe","ar","ban","ana"]))
-=======
 function separateLiquids(glass) {
     let text1 = []
     let m = glass.length
@@ -71,4 +44,3 @@ return glass
 
 let glass = [['A', 'A', 'O', 'H'], ['A', 'H', 'W', 'O'], ['W', 'W', 'A', 'W'], ['H', 'H', 'O', 'O']]
 separateLiquids(glass)
->>>>>>> a14ce7312553e4a1d3c319411f8e3a215301554a
