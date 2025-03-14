d22a91011de3c19067db646b8575038e2b4440be
emi web developer <marianseether@gmail.com>
2025-03-14 07:27:37 +0200

    1

---
+++ z16/main.js
@@ -0,0 +1,50 @@
+// 1-10
+
+// const rec = num => {
+//     if (num > 0) {
+//         console.log(num)
+//         return rec(--num)
+//     }
+// }
+// const rec = num => {
+//    for(let i = 10; i > 0; i--) console.log(i)
+// }
+
+// console.log(rec(10))
+
+
+// 4,6,8,10
+
+// const rec = num => {
+//     if(num > 3) {
+//         console.log(num)
+//         return rec(num-2)
+//     }
+// }
+
+// console.log(rec(10))
+
+// function afiseazaNumere(num) {
+//     if (num < 11) {
+
+//     console.log(num); // Afișează numărul curent
+//     return afiseazaNumere(num + 1); // Continuă cu următorul număr
+//     }
+//   }
+
+//   afiseazaNumere(0); // Start: 1
+//   afiseazaNumere(2); // Start: 1
+//   afiseazaNumere(5); // Start: 1
+
+// function rec1(num) {
+//     if (num < 11) {
+//         console.log(num)
+//         return rec1(num + 1)
+//     }
+// }
+
+// console.log(rec1(0))
+// console.log(rec1(2))
+// console.log(rec1(5))
+
+
