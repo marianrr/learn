81cd0e35aa6f5a88b0a02ca565b71d4b615cc786
emi web developer <marianseether@gmail.com>
2025-03-15 08:53:39 +0200

    toDo1

---
+++ p3_toDo/main.js
@@ -0,0 +1,38 @@
+const form4 = document.getElementById("submitForm")
+const input3 = document.getElementById("input1")
+const ul1 = document.getElementById("container")
+let notes = ["Walk in the park", "Read a book", "Go shopping in the mall", "Listen to new album"]
+const submitForm2 = (e) => {
+    e.preventDefault()
+    const form3 = new FormData(form4)
+    let note = form3.get("input1")
+    console.log("Nota este: ", note)
+
+    let li2 = document.createElement("li")
+    li2.textContent = note
+    li2.setAttribute("ondblclick", "ondblclick1(event)")
+    ul1.appendChild(li2)
+    input3.value = ""
+}
+
+const ondblclick1 = (e) => {
+    e.currentTarget.remove()
+}
+
+
+
+
+
+
+
+
+const afis = () => {
+    notes.forEach(elem => {
+        let li1 = document.createElement("li")
+        li1.setAttribute("ondblclick", "ondblclick1(event)")
+        li1.textContent = elem
+        ul1.appendChild(li1)
+    }
+    )
+}
+afis()
