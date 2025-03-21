613e879f9c6ff142b3bb5c9678e8f3768dccaaf0
marianrr <marianseether@gmail.com>
2025-03-21 20:21:34 +0200

    th

---
+++ this/main.js
@@ -0,0 +1,30 @@
+// console.log(this)
+
+// const gi = {
+//     name: "kikila",
+//     name1() {
+//         console.log(this)
+//         return "Yes"
+//     }
+// }
+
+// console.log(gi.name1())
+
+// function aa() {
+//     console.log(this)
+// }
+// (function ab() {
+//     console.log(this)
+// })()
+
+// aa()
+// console.log(this)
+
+// (function() {
+//     "use strict"
+//     console.log(this)
+// }())
+
+
+// arrow functions
+
