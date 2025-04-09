0c8b891cf2ae1c2004b09b50af55607148ae598d
marianrr <marianseether@gmail.com>
2025-04-09 10:00:16 +0300

    proiect react Weather app

---
+++ 0projects/reactProjects/Weather/routes/Home.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import path from 'path'
+const __dirname = import.meta.dirname
+const router = express.Router()
+router.use(express.static('public'))
+
+router.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '..', 'public', 'home.html'))
+})
+
+export default router
